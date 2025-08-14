35c5a23a0a3895361b9c3beb801e91cfa659efcb
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-08-15 04:05:20 +0530

    Modify service file with edit-employee component

--- EmployeeApp.UI/src/app/components/employees/edit-employee/edit-employee.ts
+++ EmployeeApp.UI/src/app/components/employees/edit-employee/edit-employee.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeesService } from '../../../services/employees.service';
+import { Employee } from '../../../models/employee.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-employee',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-employee.html',
-  styleUrl: './edit-employee.css'
+  styleUrls: ['./edit-employee.css']
 })
-export class EditEmployee {
+export class EditEmployee implements OnInit {
+  employeeDetails: Employee={
+    id:'',
+    name:'',
+    email:'',
+    phone:0,
+    salary:0,
+    department:''
+  };
 
+  constructor(private route: ActivatedRoute, private employeesService: EmployeesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id');
+   
+    if (id) {
+      this.employeesService.getEmployee(id).subscribe({
+        next:(response)=>{
+          this.employeeDetails=response;
+        }
+      });
+    }
+   }
+  });
+ }
+
+ updateEmployee(){
+  this.employeesService.updateEmployee(this.employeeDetails.id,this.employeeDetails)
+  .subscribe({
+    next: (response) => {
+      this.router.navigate(['/employees']);
+    },
+    error: (error) => {
+      console.error('Error updating employee:', error);
+    }
+  });
+ }
+
+ deleteEmployee(id:string){
+  this.employeesService.deleteEmployee(id)
+  .subscribe({
+    next: (response) => {
+      this.router.navigate(['/employees']);
+    },
+    error: (error) => {
+      console.error(error);
+    }
+  });
+ }
 }
--- EmployeeApp.UI/src/environments/environment.ts
+++ EmployeeApp.UI/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseApiUrl: 'https://localhost:7107/api'  
+  baseApiUrl: 'https://localhost:7107'  
 };
