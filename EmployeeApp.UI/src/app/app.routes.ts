dcf84826e9f627cc4833fd1963196d232862bf88
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-08-15 02:33:19 +0530

    Create add-employee to the header and arrange the path in route file

--- EmployeeApp.UI/src/app/app.routes.ts
+++ EmployeeApp.UI/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { EmployeesList } from './components/employees/employees-list/employees-list';
+import { AddEmployee } from './components/employees/add-employee/add-employee';
+import { EditEmployee } from './components/employees/edit-employee/edit-employee';
 export const routes: Routes = [
     {
         path: '',
@@ -8,5 +10,13 @@ export const routes: Routes = [
     {
         path: 'employees',
         component : EmployeesList
+    },
+    {
+        path: 'employees/add',
+        component : AddEmployee
+    },
+    {
+        path: 'employees/edit/:id',
+        component : EditEmployee
     }
 ];
